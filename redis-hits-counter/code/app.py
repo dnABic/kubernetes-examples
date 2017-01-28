8e02422a3184e269984627f0c67c4a886c33bd3c
dnABic (Andreja Babic) <ababic@gmail.com>
2017-01-28 13:34:58 +0100

    Adding readiness and health url

--- redis-hits-counter/code/app.py
+++ redis-hits-counter/code/app.py
@@ -9,5 +9,13 @@ def hello():
     count = redis.incr('hits')
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
+@app.route('/health')
+def health():
+    return 'All fine'
+
+@app.route('/readiness')
+def readiness():
+    return 'I am ready'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
